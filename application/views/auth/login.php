e3e0abacf803f6cae762df32467224324b8da929
Aaron Bill Domingo <domingoaaronbill@gmail.com>
2019-10-09 20:04:57 +0800

    first commit

---
+++ application/views/auth/login.php
@@ -0,0 +1,15 @@
+<form action='/logincheck' method='post'>
+<div class="form-group">
+  <label for="email">Email</label>
+  <input type="email" class="form-control" name="" id="" aria-describedby="emailHelpId" placeholder="Email">
+  <small id="emailHelpId" class="form-text text-muted">Help text</small>
+</div>
+
+<div class="form-group">
+  <label for="password">Password</label>
+  <input type="password" class="form-control" name="" id="" placeholder="Password">
+</div>
+
+<button type="submit" class="btn btn-primary">Submit</button>
+
+<form>
---
+++ application/views/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel = "stylesheet" type = "text/css" 
+    href = "<?php echo base_url(); ?>css/bootstrap.min.css">
+
+    <script type = 'text/javascript' src = "<?php echo base_url(); 
+    ?>js/bootstrap.min.js"></script>
+    <script type = 'text/javascript' src = "<?php echo base_url(); 
+    ?>js/jquery min-3.3.1.js"></script>
+    <title><?php echo $title ?> </title>
+</head>
+<body>
